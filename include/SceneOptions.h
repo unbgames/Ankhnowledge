7ed0b19d55482b7186237a914969781eaeec5b64
Arthur Del Esposte <arthurmde@yahoo.com.br>
2013-12-11 00:27:14 -0200

    SceneOptions.h

--- include/SceneOptions.h
+++ include/SceneOptions.h
@@ -7,6 +7,7 @@
 
 #ifndef SCENEOPTIONS_H_
 #define SCENEOPTIONS_H_
+#include "Animation.h"
 #include "Scene.h"
 #include "Sprite.h"
 #include "SplashScreen.h"
@@ -20,11 +21,15 @@ class SceneOptions : public Scene {
 public:
 	SceneOptions(string sceneName);
 	virtual ~SceneOptions();
+	virtual void render(float cameraX, float cameraY);
+	virtual int update(int dt);
 	void Enter();
 	void Exit();
 
 private:
 	OptionsController* oc;
+	Sprite* sound;
+	Animation* soundAnimation;
 };
 
 #endif /* SCENEOPTIONS_H_ */
